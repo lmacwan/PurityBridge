6da89c752d3a0a10e1ac081fe7886a95fb29b3ea
lmacwan <lmacwan@hotmail.com>
2014-08-01 15:24:41 +0530

    testibg

--- PurityBridge.Live/Controllers/TreatmentsController.cs
+++ PurityBridge.Live/Controllers/TreatmentsController.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Umbraco.Web.Mvc;
+using Umbraco.Web.Models;
 
 namespace PurityBridge.Live
 {
     public class TreatmentsController : RenderMvcController
     {
-        public override ActionResult Index(Umbraco.Web.Models.RenderModel model)
+        public override ActionResult Index(RenderModel model)
         {
             return base.Index(model);
         }
