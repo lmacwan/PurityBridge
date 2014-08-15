0e1b9c6cac60d779f0d68a1fe23a52c5b25b1d55
lmacwan <lmacwan@hotmail.com>
2014-08-15 12:28:31 +0530

    Uploaded To Umbraco.kentec

---
+++ PurityBridge.Live/Controllers/GalleryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace PurityBridge.Live.Controllers
+{
+    public class GalleryController : RenderMvcController
+    {
+        public override ActionResult Index(RenderModel model)
+        {
+            return base.Index(model);
+        }
+
+        //public ActionResult Treatment(RenderModel model)
+        //{
+        //    return View(model);
+        //}
+    }
+}
