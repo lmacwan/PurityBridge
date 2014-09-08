337b1019a1ce14d80b3dfc29b385ef8ffd9fc999
lmacwan <lmacwan@hotmail.com>
2014-09-08 21:06:41 +0530

    Sub Menu palce in Treatments index

--- PurityBridge.Umbraco/scripts/Common.js
+++ PurityBridge.Umbraco/scripts/Common.js
@@ -76,6 +76,18 @@ function getPhoto(photoId) {
     });
 }
 
+jQuery.cachedScript = function (url, options) {
+    // Allow user to set any option except for dataType, cache, and url
+    options = $.extend(options || {}, {
+        dataType: "script",
+        cache: true,
+        url: url
+    });
+    // Use $.ajax() since it is more flexible than $.getScript
+    // Return the jqXHR object so we can chain callbacks
+    return jQuery.ajax(options);
+};
+
 $(window).load(function () {
     $('#ui-datepicker-div').addClass('widget-calendar');
 });
---
+++ PurityBridge.Umbraco/scripts/categories.js
@@ -0,0 +1,14 @@
+﻿/**/
+/* treatments carousel */
+/**/
+$('#treatments-carousel').owlCarousel({
+    items: 4,
+    itemsDesktop: false,
+    itemsDesktopSmall: false,
+    itemsTablet: [980, 4],
+    itemsTabletSmall: [767, 3],
+    itemsMobile: [479, 1],
+    navigation: true,
+    navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    pagination: false
+});
