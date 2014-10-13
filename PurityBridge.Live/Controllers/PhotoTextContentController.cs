bbfd97920b66a10e7327464da33f496dddee221d
lmacwan <lmacwan@hotmail.com>
2014-10-13 23:46:24 +0530

    initial commit

---
+++ PurityBridge.Live/Controllers/PhotoTextContentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace PurityBridge.Live
+{
+    public class PhotoTextContentController : RenderMvcController
+    {
+        public override ActionResult Index(RenderModel model)
+        {
+            var breadcrumbs = new List<BreadCrumbElement>();
+
+            breadcrumbs.Add(new BreadCrumbElement()
+            {
+                Name = model.Content.Name,
+                Value = "/" + model.Content.UrlName
+            });
+
+            ViewBag.BreadCrumbs = breadcrumbs;
+            return base.Index(model);
+        }
+
+        public ActionResult TheRightChoice(RenderModel model, string category)
+        {
+            var breadcrumbs = new List<BreadCrumbElement>();
+
+            breadcrumbs.Add(new BreadCrumbElement()
+            {
+                Name = model.Content.Name,
+                Value = "/" + model.Content.UrlName
+            });
+
+            ViewBag.BreadCrumbs = breadcrumbs;
+            return View(model);
+        }
+
+    }
+}
