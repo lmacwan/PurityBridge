137fc915fb51a196fc677190bc37dbce4a192319
Leon Lappy <lmacwan@hotmail.com>
2014-10-24 17:37:11 +0530

    Homepage tw image structure added

--- PurityBridge.Umbraco/scripts/spascript.js
+++ PurityBridge.Umbraco/scripts/spascript.js
@@ -19,10 +19,13 @@ $(document).ready(function () {
     $(function () {
         $('.tabs').delegate('li:not(.active)', 'click', function () {
             $(this).addClass('active').siblings().removeClass('active').parents('.tab').find('.box').hide().eq($(this).index()).fadeIn(250);
+            $('.mask').first().html($($('.tabs li.active').parents('.tab').find('.box')[$('.tabs li.active').index()]).find('img').clone());
         })
-        var total = $('.tabs li').length;
+
+        $('.mask').first().html($($('.tabs li.active').parents('.tab').find('.box')[$('.tabs li.active').index()]).find('img').clone());
+
         setInterval(function () {
-            $($('.tabs li')[($('.tabs li.active').index() + 1) % total]).addClass('active').siblings().removeClass('active').parents('.tab').find('.box').hide().eq($('.tabs li.active').index()).fadeIn(250);
+            changeImageInterval();
         }, 5000);
     });
 
@@ -35,6 +38,12 @@ $(document).ready(function () {
             $next.click();
             return false;
         });
+    }
+
+    function changeImageInterval() {
+        var total = $('.tabs li').length;
+        $($('.tabs li')[($('.tabs li.active').index() + 1) % total]).addClass('active').siblings().removeClass('active').parents('.tab').find('.box').hide().eq($('.tabs li.active').index()).fadeIn(250);
+        $('.mask').first().html($($('.tabs li.active').parents('.tab').find('.box')[$('.tabs li.active').index()]).find('img').clone());
     }
 
     //------------------------------------------------------------------------//
